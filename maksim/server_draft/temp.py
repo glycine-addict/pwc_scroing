1920a1fc2eb661601dc619f47fe2d4b124087f59
Maksim Pukin <63252017+glycine-addict@users.noreply.github.com>
2020-05-07 18:37:06 +0300

    template

--- maksim/server_draft/temp.py
+++ maksim/server_draft/temp.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from flask import render_template
+import pandas as pd
 
+data = pd.read_csv('rosstat7y.csv')
+
+data = data.iloc[0, :]
+data = data.to_dict()
+result = {}
+for i in data:
+    if(data[i] != 0):
+        result[i] = data[i]
+name = result['0']
+inn = result['1']
+result.pop('0')
+result.pop('1')
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/result')
 def index_page():
-    return render_template('hello.html')
+    return render_template('template.html', name = name,
+                           inn = inn, result = result)
 
 if __name__ == '__main__':
     app.run()
